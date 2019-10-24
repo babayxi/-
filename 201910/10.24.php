e84314983f9cde44bc8fa4fb26fe296f02c57b07
babayxi <56524969+babayxi@users.noreply.github.com>
2019-10-24 15:44:12 +0800

    Create 10.24.php

---
+++ 201910/10.24.php
@@ -0,0 +1,9 @@
+             <form action="http://www.baidu.com/baidu" target="_blank">
+                        <input id="kw" type="text" baidu Sug="1" size="30" name="word">
+                        <input type="submit" value="百度">
+                    </form>
+                    <script charset="gbk" src="http://www.baidu.com/js/opensug.js">
+                        
+                    </script>
+
+
